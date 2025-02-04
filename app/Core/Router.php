c489b15bb8b63c9303f88816896501da91b88946
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-02-05 00:06:10 +0100

    j'ai modifier le nom du  class app vers router

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -2,7 +2,7 @@
 
 namespace App\Core;
 
-class App 
+class Router 
 {
     protected $controller = "HomeController";
     protected $action = "index";
--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php 
  require dirname( __DIR__) . '/vendor/autoload.php';
- use  App\Core\App;
- new App();
+ use  App\Core\Router;
+ new Router();
 
 
 ?>

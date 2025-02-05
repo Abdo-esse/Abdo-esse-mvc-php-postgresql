d72ac4898d8315f90f88733e24721ea31c6c2b22
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-02-05 13:14:34 +0100

    J'ai modefier la connexion de data base au .env

--- app/Config/Connexion.php
+++ app/Config/Connexion.php
@@ -1,39 +1,40 @@
-<?php 
+<?php
 namespace App\Config;
+
+require dirname(__DIR__) . '/../vendor/autoload.php'; 
+
 use PDO;
-use PDOExeption;
+use PDOException;
+use Dotenv\Dotenv;
+
 class Connexion
 {
-    private static $host='localhost';
-    private static $db='test';
-    private static $username='postgres';
-    private static $password='root';
-     public static $conn=null;
+    private static $conn = null;
 
     static function connexion()
     {
-        if(self::$conn===null)
-        {
-            try{
-                self::$conn= new PDO("pgsql:host=".self::$host.";dbname=".self::$db,
-                self::$username,
-                self::$password,
-            );
-            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected to postgresql";
-            }catch(PDOExeption $exeption)
-            {
-                die ( " L'error est " . $exeption->getMessage());
+        if (self::$conn === null) {
+            try {
+                
+                $dotenv = Dotenv::createImmutable(dirname(__DIR__,2));
+                $dotenv->load();
+
+                
+                $host = $_ENV['DATABASE_HOSTNAME'] ?? 'localhost';
+                $db = $_ENV['DATABASE_NAME'] ?? 'test';
+                $username = $_ENV['DATABASE_USERNAME'] ?? 'postgres';
+                $password = $_ENV['DATABASE_PASSWORD'] ?? 'root';
+
+                
+                self::$conn = new PDO("pgsql:host=$host;dbname=$db", $username, $password);
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                echo "Connected to PostgreSQL"; 
+
+            } catch (PDOException $exception) {
+                die("Erreur de connexion : " . $exception->getMessage());
             }
-            return self::$conn;
-
         }
-        else
-        {
-            return self::$conn;
-        }
-
+        return self::$conn;
     }
-
-
 }
